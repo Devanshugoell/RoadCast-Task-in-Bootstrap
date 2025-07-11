86ad8043bb80bab898eb706a931f11ec02964756
Devanshu <devgoyal952@gmail.com>
2025-07-11 14:04:58 +0530

    Work together, wherever you work  made responsive

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const buttons = document.querySelectorAll(".filter-btn");
 const buttons2 = document.querySelectorAll(".filter-btn2");
+const inputValue = document.getElementById("inputValue").value;
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
@@ -14,3 +15,17 @@ buttons2.forEach((btn) => {
     btn.classList.add("bg-F1F3FD");
   });
 });
+
+const handleSignUp = () => {
+  const inputValue = document.getElementById("inputValue").value;
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailPattern.test(inputValue)) {
+    alert(
+      `Thank you ${inputValue} , We have recieved your mail and will contact you shortly`
+    );
+  } else {
+    alert("Please enter a valid email address.");
+  }
+};
