ee2622245797560c3d79bc621b7f6848ad80050f
Devanshu <devgoyal952@gmail.com>
2025-07-11 12:00:03 +0530

    Added filter button functionality

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const buttons = document.querySelectorAll(".filter-btn");
+const buttons2 = document.querySelectorAll(".filter-btn2");
 
 buttons.forEach((btn) => {
   btn.addEventListener("click", () => {
     buttons.forEach((b) => b.classList.remove("bg-F1F3FD"));
+    btn.classList.add("bg-F1F3FD");
+  });
+});
+
+buttons2.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    buttons2.forEach((b) => b.classList.remove("bg-F1F3FD"));
     btn.classList.add("bg-F1F3FD");
   });
 });
